afee515fa4fb1e2912ba066c8f1b061dd54cc6bc
pandaagni <agnipanda97@gmail.com>
2025-06-07 05:47:14 +0530

    store token and userId to localStorage

--- src/Services/Login.ts
+++ src/Services/Login.ts
@@ -16,7 +16,10 @@ export const Login = async (formData: FormData) => {
         if (!response.ok) {
             throw new Error("Network response was not ok");
         }
-        return await response.json();
+        const res = await response.json();
+        localStorage.setItem("token", res.token);
+        localStorage.setItem("userId", res.userId);
+        return res;
     } catch (error) {
         console.error("There was a problem with the fetch operation:", error);
         throw error;
